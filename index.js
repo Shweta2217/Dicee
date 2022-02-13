b58e56f5b20e017969dd872dc794dca7dbddf613
Shweta <shwetasaini2217@gmail.com>
2022-02-13 23:35:11 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-setTimeout(() => {
-    greets
-}, 5000);
-function greets(params) {
 
-    let randomNumber1 = Math.floor(Math.random() * (6 - 1) + 1);
-    let randomNumber2 = Math.floor(Math.random() * (6 - 1) + 1);
+let randomNumber1 = Math.floor(Math.random()* (6 - 1) + 1);
+let randomNumber2 = Math.floor(Math.random()* (6 - 1) + 1);
 
-    document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png")
-    document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png")
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png")
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png")
 
-    if (randomNumber1 > randomNumber2) {
-        document.querySelector(".heading").innerHTML = "Player1 Won";
-    }
-    else if (randomNumber2 > randomNumber1) {
-        document.querySelector(".heading").innerHTML = "Player2 Won";
-    }
-    else if (randomNumber1 === randomNumber2) {
-        document.querySelector(".heading").innerHTML = "Draw!";
-    }
+if(randomNumber1 > randomNumber2){
+    document.querySelector(".heading").innerHTML = "Player1 Won!";  
+} 
+else if(randomNumber2 > randomNumber1){
+    document.querySelector(".heading").innerHTML = "Player2 Won!";  
 }
+else if (randomNumber1 === randomNumber2){
+document.querySelector(".heading").innerHTML = "Draw!";   
+} 
 
 
